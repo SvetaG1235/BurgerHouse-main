a9854b264a9e9b39beb9f215f847127288815b37
Catmintmintmint <svetikp386@gmail.com>
2025-02-28 09:26:23 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import usersRouter from './routes/users.js'
 import registerRouter from './routes/register.js'
 import cartRouter from './routes/cart.js'
 import dishesRouter from './routes/dishes.js'
-import adminRouter from "./routes/admin.js";
+import UsersModels from './models/UsersModels.js'
+import bcrypt from 'bcrypt';
 
 export const app = express();
 const __dirname = path.resolve()
@@ -24,7 +25,7 @@ app.use('/users', usersRouter);
 app.use('/auth', registerRouter);
 app.use('/cart', cartRouter);
 app.use('/dishes', dishesRouter);
-app.use('/admin', adminRouter);
+
 
 try{
     sequelizeDB.sync()
@@ -32,5 +33,38 @@ try{
     console.log('Упс! База не живая:(')
 }
 
+app.post('/register', async (req, res) => {
+    const { name, phone, email, username, password, confirmPassword, age, role } = req.body;
 
+    // Проверка на совпадение паролей
+    if (password !== confirmPassword) {
+        return res.status(400).send('Пароли не совпадают');
+    }
+
+    try {
+        // Хеширование пароля
+        const hashedPassword = await bcrypt.hash(password, 10); 
+
+        // Создание нового пользователя
+        const newUser   = await UsersModels.create({
+            name,
+            phone, 
+            email, 
+            username,
+            password: hashedPassword, 
+            role: role, 
+            age: age 
+        });
+
+        res.status(201).send(`
+            <h1>Пользователь успешно зарегистрирован</h1>
+            <p><a href="auth/enter">Перейти на страницу входа</a></p>
+        `);
+    } catch (error) {
+        console.error('Ошибка при регистрации пользователя:', error);
+        res.status(500).send('Ошибка сервера');
+    }
+});
 export default app
+
+//npm install express sqlite3 bcrypt jsonwebtoken dotenv
