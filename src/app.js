5c44bd46abcdc36c8e69777bf111e548457541c6
SvetaG1235 <svetikp386@gmail.com>
2025-03-15 22:10:51 +0700

    Помогите

--- src/app.js
+++ src/app.js
@@ -13,25 +13,30 @@ import dishRouter from './routes/DishRoutes.js';
 const app = express();
 const __dirname = path.resolve();
 
+// Настройка middleware
 app.use(logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'src/public/stylesheets'))); 
 
+// Настройка шаблонизатора
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'src/views'));
 
-app.get('Web App BH entered', (req, res) => {
-    res.render('Web_App_BH_entered.hbs', { title: 'Добро пожаловать' });
+// Маршрут для страницы "web-app-bh-entered"
+app.get('/web-app-bh-entered', (req, res) => {
+    res.render('web-app-bh-entered.hbs', { title: 'Добро пожаловать' });
 });
 
+// Подключение маршрутов
 app.use('/', indexRouter); 
 app.use('/auth', authRouter); 
 app.use('/cart', cartRouter); 
 app.use('/dishes', dishRouter); 
 //app.use('/admin', adminRouter); 
 
+// Синхронизация базы данных
 try {
     sequelizeDB.sync(); 
     console.log('База данных успешно синхронизирована');
